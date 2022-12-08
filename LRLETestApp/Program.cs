aaa9e8871a65aeec77ef4e43fea922c10f34fb0d
andrew <andrewtavera@outlook.com>
2022-12-08 02:41:35 -0500

    move stopwatch to separate file

--- LRLETestApp/Program.cs
+++ LRLETestApp/Program.cs
@@ -237,32 +237,6 @@ namespace LREParser
             return bmp;
 
         }
-        class ConsoleStopWatch : IDisposable
-        {
-            private readonly string subject;
-            private readonly Stopwatch stopwatch;
-
-            public ConsoleStopWatch(string subject)
-            {
-                this.subject = subject;
-                this.stopwatch = new Stopwatch();
-                this.stopwatch.Start();
-            }
-
-            public void Dispose()
-            {
-                this.Stop();
-            }
-
-            private void Stop()
-            {
-                if (this.stopwatch.IsRunning)
-                {
-                    this.stopwatch.Stop();
-                }
-                Console.WriteLine($"{subject} finished in {(stopwatch.ElapsedMilliseconds / 1000.0)} s");
-            }
-        }
 
         private static void ParseCommandLineOptions(string[] args)
         {
